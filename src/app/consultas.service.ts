5f884c99133a163da4365234f1a442e4df278cfd
Fitonho <filipe.mouro@unesp.br>
2020-09-21 19:41:58 -0300

    métodos para responder a requisição de cada aba implementados

--- src/app/consultas.service.ts
+++ src/app/consultas.service.ts
@@ -1,102 +1,141 @@
-import { Injectable } from '@angular/core';
-import { Consulta } from './pacientes/consultas.model';
-import { CriarConsultaComponent } from './consultas/criar-consulta/criar-consulta.component';
-import { ModalController } from '@ionic/angular';
+import { Injectable } from "@angular/core";
+import { Consulta } from "./pacientes/consultas.model";
+import { CriarConsultaComponent } from "./consultas/criar-consulta/criar-consulta.component";
+import { ModalController } from "@ionic/angular";
+import { BehaviorSubject } from "rxjs";
+import { map } from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ConsultasService {
-  private _consultas: Consulta[] = [
+  private _consultas = new BehaviorSubject<Consulta[]>([
     new Consulta(
-      'Filipe',
-      'Paciente 1',
-      new Date(2020, 4, 28, 22),
-      '49,99',
+      "Filipe",
+      "Paciente 1",
+      new Date(),
+      "49.99",
       new Date(2020, 6, 30),
-      '0',
-      'Pendente de pagamento',
-      0,0,0
+      "0",
+      "Pendente de pagamento",
+      0,
+      0,
+      0
     ),
     new Consulta(
-      'Filipe',
-      'Paciente 1',
-      new Date(2020, 4, 30, 14),
-      '49,99',
+      "Filipe",
+      "Paciente 1",
+      new Date(),
+      "49.99",
       new Date(2020, 7, 30),
-      '0',
-      'Pendente de pagamento',
-      0,0,0
+      "0",
+      "Pendente de pagamento",
+      0,
+      0,
+      0
     ),
     new Consulta(
-      'Filipe',
-      'Paciente 1',
+      "Filipe",
+      "Paciente 1",
       new Date(2020, 5, 3, 21),
-      '49,99',
+      "49.99",
       new Date(2020, 8, 30),
-      '0',
-      'Pendente de pagamento',
-      0,0,0
+      "0",
+      "Pendente de pagamento",
+      0,
+      0,
+      0
     ),
     new Consulta(
-      'Filipe',
-      'Paciente 3',
-      new Date(2020, 5, 27, 16),
-      '49,99',
+      "Filipe",
+      "Paciente 3",
+      new Date(2020, 8, 24, 16),
+      "49.99",
       new Date(2020, 5, 30),
-      '0',
-      'Pendente de pagamento',
-      0,0,0
+      "0",
+      "Pendente de pagamento",
+      0,
+      0,
+      0
     ),
     new Consulta(
-      'Filipe',
-      'Paciente 2',
+      "Filipe",
+      "Paciente 2",
       new Date(2020, 5, 27, 16),
-      '49,99',
+      "49.99",
       new Date(2020, 5, 30),
-      '0',
-      'Pendente de pagamento',
-      0,0,0
+      "0",
+      "Pendente de pagamento",
+      0,
+      0,
+      0
     ),
-  ]
+  ]);
 
-  constructor(
-    private modalCtrl:ModalController
-  ) { }
+  constructor(private modalCtrl: ModalController) {}
 
   get consultas() {
-    return [...this._consultas]
+    return this._consultas.asObservable();
   }
 
-  getTodayConsultas() {
+  getConsultasHoje() {
     let today = new Date();
-    return this.consultas.filter((consulta) => {
-      return consulta.dataConsulta.getDate() == today.getDate() &&
-        consulta.dataConsulta.getMonth() == today.getMonth() &&
-        consulta.dataConsulta.getFullYear() == today.getFullYear()
-    })
+    return this.consultas.pipe(
+      map((consultas) => {
+        return consultas.filter((consulta) => {
+          return (
+            consulta.dataConsulta.getDate() == today.getDate() &&
+            consulta.dataConsulta.getMonth() == today.getMonth() &&
+            consulta.dataConsulta.getFullYear() == today.getFullYear()
+          );
+        });
+      })
+    );
   }
 
   getProximasConsultas() {
     let today = new Date();
-    let nextWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 7);
-    return this.consultas.filter((consulta) => {
-      return today.getTime() <= consulta.dataConsulta.getTime() &&
-        consulta.dataConsulta.getTime() <= nextWeek.getTime()
-    })
+    today.setHours(0, 0, 0, 0);
+    let nextWeek = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate() + 7
+    );
+    return this.consultas.pipe(
+      map((consultas) => {
+        return consultas.filter((consulta) => {
+          return (
+            today.getTime() <= consulta.dataConsulta.getTime() &&
+            consulta.dataConsulta.getTime() <= nextWeek.getTime() &&
+            consulta.dataConsulta.getTime() >= today.getTime()
+          );
+        });
+      })
+    );
   }
 
-  openCreateAppointmentForm(){
-    this.modalCtrl.create({
-      component:CriarConsultaComponent
-    })
-    .then(modalElement => {
-      modalElement.present();
-      return modalElement.onDidDismiss();
-    })
-    //ainda ta em formato de forms. arrumar pra pegar só o conteúdo necessário
-    .then(personCreated =>{
-      console.log(personCreated.data,personCreated.role);
-    })
+  getConsultasPendentes(){
+    return this.consultas.pipe(
+      map((consultas) => {
+        return consultas.filter((consulta) => {
+          return Number(consulta.valorPago) < Number(consulta.valor)
+        });
+      })
+    );
+  }
+
+  openCreateAppointmentForm() {
+    this.modalCtrl
+      .create({
+        component: CriarConsultaComponent,
+      })
+      .then((modalElement) => {
+        modalElement.present();
+        return modalElement.onDidDismiss();
+      })
+      //ainda ta em formato de forms. arrumar pra pegar só o conteúdo necessário
+      .then((personCreated) => {
+        console.log(personCreated.data, personCreated.role);
+      });
   }
 }
