a045cff4910bc069635cf1244e76f550658aa04b
Fitonho <filipe.mouro@unesp.br>
2020-09-21 19:43:16 -0300

    esqueleto de autenticação criado

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth/auth.guard';
 import { TabsPage } from './tabs.page';
 
 const routes: Routes = [
@@ -9,36 +10,44 @@ const routes: Routes = [
     children: [
       {
         path: 'pacientes',
-        loadChildren: () => import('../pacientes/pacientes.module').then( m => m.PacientesPageModule)
+        loadChildren: () => import('../pacientes/pacientes.module').then( m => m.PacientesPageModule),
+        canLoad: [AuthGuard]
       },
       {
         path: 'consultas-hoje',
-        loadChildren: () => import('../consultas/hoje/consultas-hoje.module').then( m => m.ConsultasHojePageModule)
+        loadChildren: () => import('../consultas/hoje/consultas-hoje.module').then( m => m.ConsultasHojePageModule),
+        canLoad: [AuthGuard]
       },
       {
         path: 'consultas-proximas',
-        loadChildren: () => import('../consultas/proximas/consultas-proximas.module').then( m => m.ConsultasProximasPageModule)
+        loadChildren: () => import('../consultas/proximas/consultas-proximas.module').then( m => m.ConsultasProximasPageModule),
+        canLoad: [AuthGuard]
       },
       {
         path: 'consultas-todas',
-        loadChildren: () => import('../consultas/todas/consultas-todas.module').then( m => m.ConsultasTodasPageModule)
+        loadChildren: () => import('../consultas/todas/consultas-todas.module').then( m => m.ConsultasTodasPageModule),
+        canLoad: [AuthGuard]
       },
       {
         path: 'relatorio',
-        loadChildren: () => import('../relatorio/relatorio.module').then( m => m.RelatorioPageModule)
+        loadChildren: () => import('../relatorio/relatorio.module').then( m => m.RelatorioPageModule),
+        canLoad: [AuthGuard]
       },
       {
         path: 'totais',
-        loadChildren: () => import('../totais/totais.module').then( m => m.TotaisPageModule)
+        loadChildren: () => import('../totais/totais.module').then( m => m.TotaisPageModule),
+        canLoad: [AuthGuard]
       },
       {
         path: 'pendente',
-        loadChildren: () => import('../consultas/pendente/pendente.module').then( m => m.PendentePageModule)
+        loadChildren: () => import('../consultas/pendente/pendente.module').then( m => m.PendentePageModule),
+        canLoad: [AuthGuard]
       },
       {
         path: '',
         redirectTo: '/tabs/pacientes',
-        pathMatch: 'full'
+        pathMatch: 'full',
+        canLoad: [AuthGuard]
       }
     ]
   },
