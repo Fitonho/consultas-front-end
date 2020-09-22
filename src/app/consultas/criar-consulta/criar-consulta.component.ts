43dc65b39457c6c9adef806c65b5900e1add5abb
Fitonho <filipe.mouro@unesp.br>
2020-09-22 01:45:25 -0300

    formulário alterado para ter um possível valor padrão formulário retornando nome do paciente ao invés de índice

--- src/app/consultas/criar-consulta/criar-consulta.component.ts
+++ src/app/consultas/criar-consulta/criar-consulta.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { PacientesService } from '../../pacientes.service';
 import { Paciente } from '../../paciente.model';
 import { ModalController } from '@ionic/angular';
+import { Consulta } from 'src/app/pacientes/consultas.model';
 
 @Component({
   selector: 'app-criar-consulta',
@@ -11,42 +12,44 @@ import { ModalController } from '@ionic/angular';
 })
 export class CriarConsultaComponent implements OnInit {
 
+  @Input() consulta: Consulta = new Consulta(null,null,null,null,null,null,null);
   form: FormGroup;
-  pacientes: Paciente[]
+  pacientes: Paciente[];
 
   constructor(
     private pacientesService:PacientesService,
     private modalCtrl:ModalController
   ) {}
 
+  //TODO: transformar o input de paciente em uma textBox
   ngOnInit() {
     this.form = new FormGroup({
       pacienteIndex: new FormControl(null,{
         updateOn:'change',
         validators:[Validators.required]
       }),
-      preço: new FormControl(null,{
+      preço: new FormControl(this.consulta.preco,{
         updateOn:'change',
-        validators:[Validators.required,Validators.maxLength(180)]
+        validators:[Validators.required,Validators.maxLength(180),Validators.min(1)]
       }),
-      pago:new FormControl(null,{
+      pago:new FormControl(this.consulta.pago,{
         updateOn:'change',
-        validators:[Validators.required,Validators.min(1)]
+        validators:[Validators.required,Validators.min(0)]
       }),
-      dataConsulta: new FormControl(null,{
-        updateOn:'change',
-        validators:[Validators.required]
-      }),
-      horaConsulta: new FormControl(null,{
+      dataConsulta: new FormControl(this.consulta.dataConsulta?.toISOString(),{
         updateOn:'change',
         validators:[Validators.required]
       }),
-      dataPagamento: new FormControl(null,{
+      horaConsulta: new FormControl(this.consulta.dataConsulta?.toISOString(),{
+        updateOn:'change',
+        validators:[Validators.required]
+      }),
+      dataPagamento: new FormControl(this.consulta.dataPagamento?.toISOString(),{
         updateOn:'change',
         validators:[Validators.required]
       })
     })
-
+    console.log(this.consulta);
     this.pacientes = this.pacientesService.getPacientes();
   }
 
@@ -63,13 +66,13 @@ export class CriarConsultaComponent implements OnInit {
     let hourDate = new Date(this.form.value.horaConsulta)
     dayDate.setHours(hourDate.getHours())
     dayDate.setMinutes(hourDate.getMinutes())
-
     this.modalCtrl.dismiss({
-      paciente: this.form.value.pacienteIndex
+      paciente: this.pacientes[this.form.value.pacienteIndex].nome
       ,preco: this.form.value.preço
       ,pago: this.form.value.pago
       ,dataConsulta: dayDate.toISOString()
       ,dataPagamento: this.form.value.dataPagamento
+      ,id:this.consulta.id
     },'confirm')
   }
 }
